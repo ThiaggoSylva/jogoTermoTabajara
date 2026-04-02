7b8d995bc38f241db819c29e6acd9201b081d9ed
ThiaggoSylva <thiaggol.sylva@gmail.com>
2026-04-02 16:21:41 -0300

    first commit

---
+++ termo.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace Termo.ConsoleApp;
+
+class Program
+{
+    const int QuantidadeMaximaTentativas = 5;
+    const int TamanhoPalavra = 5;
+
+    static void Main(string[] args)
+    {
+        string[] palavras =
+        {
+            "TERMO",
+            "LIVRO",
+            "PRATO",
+            "TIGRE",
+            "NUVEM",
+            "CASAL",
+            "FESTA",
+            "PRAIA",
+            "MOUSE",
+            "VERDE"
+        };
+    }
+}
